1ddf3e5cce61565f1175811c8f0de8887c351c95
jonnyDeep <jonnyDeep@gmail.com>
2023-11-27 20:51:22 +0800

    feat: 189
    description：

--- src/rotate/Solution.java
+++ src/rotate/Solution.java
@@ -16,19 +16,25 @@ public class Solution {
 //    }
 
     public void rotate(int[] nums,int k){
-        int pre = 0,count =0;
-        int next =(pre+k)>=nums.length?(pre+k)%nums.length:pre+k;
-        int tmp = nums[pre];
-        while (count<nums.length){
-            nums[next] = tmp;
-            tmp = nums[pre];
-            count++;
+        k %=nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }
+
+    public void reverse(int[] nums,int start,int end){
+        while (start<end){
+            int tmp = nums[start];
+            nums[start]  =nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
         }
     }
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = new int[]{1,2,3,4,5,6,7};
-        s.rotate(nums,3);
+        int[] nums = new int[]{-1,-100,3,99};
+        s.rotate(nums,2);
         for (int num : nums) {
             System.out.println(num);
         }
